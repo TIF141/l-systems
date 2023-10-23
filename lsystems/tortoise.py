b45fa0893eca6231702a63334c9db6648cc48f9f
Douglas Brown <21172144+TIF141@users.noreply.github.com>
2023-10-23 12:16:45 +0100

    Add rotate to tortoise

--- lsystems/tortoise.py
+++ lsystems/tortoise.py
@@ -5,12 +5,18 @@ import numpy as np
 class Tortoise:
     def __init__(self):
         self.pos = np.array([0, 0])
-        self.angle = 0.0
+        self.angle_deg = 0.0
         self.d = 1.0
+
+    def get_angle_rad(self):
+        return self.angle_deg * np.pi / 180
 
     def forward(self):
         x, y = self.pos[0], self.pos[1]
-        new_x = x + self.d * np.cos(self.angle)
-        new_y = y + self.d * np.sin(self.angle)
+        new_x = x + self.d * np.cos(self.get_angle_rad())
+        new_y = y + self.d * np.sin(self.get_angle_rad())
 
         self.pos = np.array([new_x, new_y])
+
+    def rotate(self, deg):
+        self.angle_deg += deg
--- tests/test_tortoise.py
+++ tests/test_tortoise.py
@@ -1,5 +1,5 @@
 from lsystems.tortoise import Tortoise
-from numpy.testing import assert_equal, assert_array_equal
+from numpy.testing import assert_equal, assert_array_equal, assert_allclose
 import numpy as np
 
 
@@ -10,10 +10,26 @@ def test_initial_pos():
 
 def test_initial_angle():
     tortoise = Tortoise()
-    assert_equal(tortoise.angle, 0)
+    assert np.isclose(tortoise.get_angle_rad(), float(0))
 
 
 def test_forward():
     tortoise = Tortoise()
     tortoise.forward()
     assert_array_equal(tortoise.pos, np.array([1, 0]))
+
+
+def test_rotate():
+    tortoise = Tortoise()
+    angle_deg = 90
+    tortoise.rotate(angle_deg)
+    angle_rad = tortoise.get_angle_rad()
+    assert np.isclose(tortoise.get_angle_rad(), angle_rad)
+
+
+def test_rotate_and_move():
+    tortoise = Tortoise()
+    angle_deg = 45
+    tortoise.rotate(angle_deg)
+    tortoise.forward()
+    assert_allclose(tortoise.pos, np.array([1 / np.sqrt(2), 1 / np.sqrt(2)]))
