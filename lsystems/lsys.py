8a70d052eb41138a82ebc2703c6598ca7305adf0
malonej501 <jamesmalone@tutanota.com>
2023-10-23 12:21:39 +0100

    added basic step function to Lsys class

--- lsystems/lsys.py
+++ lsystems/lsys.py
@@ -4,12 +4,23 @@ class Lsys:
         self.rules = rules
 
     def add_rules(self, newrules):
-        for predecessor, sucessor in newrules.items():
-            self.rules[predecessor] = sucessor
+        for predecessor, successor in newrules.items():
+            self.rules[predecessor] = successor
+
+    def step(self, predecessors, nsteps):
+        for _ in range(nsteps):
+            successors = ""
+            for predecessor in predecessors:
+                if predecessor in self.rules:
+                    successors += self.rules[predecessor]
+            predecessors = successors
+            print(predecessors)
+        return predecessors
 
 
-test_lsys = Lsys(["a", "b"], {"A": "ABA"})
+test_lsys = Lsys(["A", "B"], {"A": "ABA"})
 print(test_lsys.rules)
 test_lsys.add_rules({"B": "BBB"})
 print(test_lsys.rules)
 print("hello")
+test_lsys.step("A", 5)
