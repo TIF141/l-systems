cf5bde88a9bcdfd98ca0dc777463b6d0be3bf875
Douglas Brown <21172144+TIF141@users.noreply.github.com>
2023-10-23 11:48:14 +0100

    Add basic tortoise functionality

--- lsystems/tortoise.py
+++ lsystems/tortoise.py
@@ -1,1 +1,16 @@
 """Class implementing turtle"""
+import numpy as np
+
+
+class Tortoise:
+    def __init__(self):
+        self.pos = np.array([0, 0])
+        self.angle = 0.0
+        self.d = 1.0
+
+    def forward(self):
+        x, y = self.pos[0], self.pos[1]
+        new_x = x + self.d * np.cos(self.angle)
+        new_y = y + self.d * np.sin(self.angle)
+
+        self.pos = np.array([new_x, new_y])
--- tests/test_tortoise.py
+++ tests/test_tortoise.py
@@ -1,11 +1,19 @@
 from lsystems.tortoise import Tortoise
 from numpy.testing import assert_equal, assert_array_equal
+import numpy as np
 
 
-def test_initial():
+def test_initial_pos():
     tortoise = Tortoise()
-    assert tortoise.pos == [0, 0]
+    assert_array_equal(tortoise.pos, np.array([0, 0]))
+
+
+def test_initial_angle():
+    tortoise = Tortoise()
+    assert_equal(tortoise.angle, 0)
 
 
 def test_forward():
     tortoise = Tortoise()
+    tortoise.forward()
+    assert_array_equal(tortoise.pos, np.array([1, 0]))
