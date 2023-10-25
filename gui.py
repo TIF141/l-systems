9de5a91c187a5ea279688a051974ce1c1efb8095
Douglas Brown <21172144+TIF141@users.noreply.github.com>
2023-10-25 13:08:50 +0100

    better interface with generate button

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6 import QtCore
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QImage
+
 from PyQt6.QtWidgets import *
 from dataclasses import dataclass
 
@@ -18,55 +19,55 @@ class Window(QWidget):
         # super().__init__()
         self.setWindowTitle("window title")
         self.setGeometry(200, 500, 400, 300)
-        # MainWindow.resize(422, 255)
-        # self.centralwidget = QWidget(MainWindow)
-        # self.createGridLayout()
 
-        # self.createGridLayout()
+        self.addRuleButton = QPushButton()
+        self.addRuleButton.setText("Add rule")
+        self.addRuleButton.clicked.connect(self.get_new_rule)
 
-        # # Want an example that lets you add an lsystem and add its rules
-        self.addLSysButton = QPushButton("word1")
-        # # self.addLSysButton.setGeometry(QtCore.QRect(160, 130, 93, 28))
+        self.rulesList = QListWidget()
+        self.rulesList.addItems([])
+        # self.rulesList.currentItemChanged.connect(self.generate)
 
-        self.generateButton = QPushButton("word2")
-        # # self.generateButton.setGeometry(QtCore.QRect(160, 110, 93, 28))
+        self.iterations = QSpinBox()
+        self.iterations.setMinimum(0)
+        # self.iterations.valueChanged.connect(self.generate)
+
+        self.generateButton = QPushButton()
+        self.generateButton.setText("Generate")
+        self.generateButton.clicked.connect(self.generate)
+        # self.rulesList.current
+
+        # self.addLSysButton = QPushButton()
+
+        # self.generateButton = QPushButton()
 
         self.label = QLabel()
-        # # self.label.setGeometry(QtCore.QRect(220, 40, 201, 111))
 
         self.label.setText("")
+        # self.addLSysButton.setText("button")
+        # self.addLSysButton.clicked.connect(self.get_input)
+        # self.generateButton.setText("generate")
+        # self.generateButton.clicked.connect(self.generate)
 
-        # MainWindow.setCentralWidget(self.centralwidget)
-        # MainWindow.setWindowTitle("example title")
-        self.addLSysButton.setText("button")
-        self.addLSysButton.clicked.connect(self.get_input)
-        self.generateButton.setText("generate")
-        self.generateButton.clicked.connect(self.generate)
-        # QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        # self.show_button = QPushButton("Show")
+        # self.show_button.clicked.connect(self.showOnLabel)
 
-        self.show_button = QPushButton("Show")
-        # self.show_button.clicked.connect(self.openSecondDialog)
-        self.show_button.clicked.connect(self.showOnLabel)
-
-        # vbox = QVBoxLayout(self)
-        # vbox.addWidget(self.addLSysButton)
-        # vbox.addWidget(self.generateButton)
-        # vbox.addWidget(self.show_button)
         self.createGridLayout()
         self.show()
 
         self.data = Data()
 
     def createGridLayout(self):
-        # self.horizontalGroupBox = QGroupBox("Grid")
         layout = QGridLayout()
-        # layout.setColumnStretch(1, 4)
-        # layout.setColumnStretch(2, 4)
 
-        layout.addWidget(self.label, 0, 0, 3, 1)
-        layout.addWidget(self.addLSysButton, 0, 1)
-        layout.addWidget(self.generateButton, 1, 1)
-        layout.addWidget(self.show_button, 2, 1)
+        layout.addWidget(self.label, 0, 0, 1, 1)
+        layout.addWidget(self.addRuleButton, 0, 1, 1, 1)
+        layout.addWidget(self.rulesList, 1, 1, 1, 1)
+        layout.addWidget(self.iterations, 2, 1, 1, 1)
+        layout.addWidget(self.generateButton, 3, 1, 1, 1)
+        # layout.addWidget(self.addLSysButton, 0, 1)
+        # layout.addWidget(self.generateButton, 1, 1)
+        # layout.addWidget(self.show_button, 2, 1)
 
         layout.setColumnStretch(0, 4)
 
@@ -76,6 +77,16 @@ class Window(QWidget):
         dialog = Dialog(self)
         dialog.show()
 
+    def get_new_rule(self):
+        rule_entry = InputDialog(self)
+        rule_entry.exec()
+        key = rule_entry.keyEntry.text()
+        value = rule_entry.valueEntry.text()
+        self.data.add_rule(key, value)
+
+        rule_str = key + " -> " + value
+        self.rulesList.addItem(rule_str)
+
     def get_input(self):
         key, done1 = QInputDialog.getText(self, "Input Dialog", "Enter rule key:")
 
@@ -84,7 +95,6 @@ class Window(QWidget):
         if done1 and done2:
             self.data.add_rule(key, rule)
             self.update_label()
-            # self.addLSysButton.hide()
 
     def update_label(self):
         s = "\n".join(str(r) + " -> " + str(k) for r, k in self.data.rules.items())
@@ -93,26 +103,19 @@ class Window(QWidget):
     def generate(self):
         assert self.data.rules is not {}
         lsys = Lsys(self.data.alphabet, self.data.rules)
-        g = Generator(lsys, "F-F-F-F", 3)
-        steps, history = g.generate_tortoise()
-        from lsystems.draw import draw_coords
+        if int(self.iterations.value()) != 0:
+            print("generating")
+            g = Generator(lsys, "F-F-F-F", self.iterations.value())
+            steps, history = g.generate_tortoise()
+            from lsystems.draw import draw_coords
 
-        from PIL import Image
-
-        im = draw_coords(history, 200)
-        im.save("image.jpg")
-
-        # self.img = QPixmap("./image.jpg")
-        # self.label = QLabel()
-        # self.label.setPixmap(self.img)
-
-        # self.grid = QGridLayout()
-        # self.grid.addWidget(self.label, 1, 1)
-        # self.setLayout(self.grid)
-
-        # app = QApplication(sys.argv)
-        # ex = Example()
-        # app.exec()
+            im = draw_coords(history, 200).copy()
+            # self.data.img = im
+            qimage = QImage(
+                im, im.shape[0], im.shape[1], QImage.Format.Format_Grayscale8
+            )
+            pixmap = QPixmap(qimage)
+            self.label.setPixmap(pixmap)
 
     def showOnLabel(self):
         pixmap = QPixmap("image.jpg")
@@ -129,9 +132,55 @@ class Data:
         self.rules[key] = value
 
 
+class InputDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.data = parent.data
+        self.createItems()
+        self.generateLayout()
+
+    def createItems(self):
+        self.keyEntry = QLineEdit()
+        self.keyEntry.setPlaceholderText("Enter rule key")
+
+        self.valueEntry = QLineEdit()
+        self.valueEntry.setPlaceholderText("Enter rule value")
+
+        # self.iterationsEntry = QSpinBox()
+        # self.iterationsEntry.setMinimum(0)
+
+        self.buttons = QDialogButtonBox(self)
+        self.buttons.setStandardButtons(
+            QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok
+        )
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+
+        # self.okButton = QPushButton()
+        # self.okButton.setText("Create rule")
+        # self.okButton.clicked.connect(self.add_rule)
+
+    def generateLayout(self):
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.keyEntry)
+        self.layout.addWidget(self.valueEntry)
+        # self.layout.addWidget(self.iterationsEntry)
+        self.layout.addWidget(self.buttons)
+        self.setLayout(self.layout)
+
+    def add_rule(self):
+        key = self.keyEntry.text
+        val = self.valueEntry.text
+        # iterations = self.iterationsEntry.value
+        self.data.add_rule(key, val)
+        self.close()
+
+
 class Dialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.data = parent.data
 
         self.image_lbl = QLabel()
         lay = QVBoxLayout(self)
@@ -140,9 +189,9 @@ class Dialog(QDialog):
         self.load_image()
 
     def load_image(self):
-        image_path = "./image.jpg"
+        image = self.data.img
         if image_path:
-            pixmap = QPixmap(image_path)
+            pixmap = QPixmap(image)
             self.image_lbl.setPixmap(QPixmap(pixmap))
 
 
