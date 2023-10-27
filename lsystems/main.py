af83803e2ff47ebc2e8f8e2523344e4e8f12b5b9
Douglas Brown <21172144+TIF141@users.noreply.github.com>
2023-10-27 12:01:13 +0100

    add base dir and icon

--- lsystems/main.py
+++ lsystems/main.py
@@ -1,9 +1,15 @@
-if __name__ == "__main__":
-    import sys
-    from PyQt6.QtWidgets import QApplication
-    from lsystems.interface.gui import Window
+import os
+import sys
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
+from lsystems.interface.gui import Window
 
+
+basedir = os.path.dirname(__file__)
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(os.path.join(basedir, "tree.ico")))
     window = Window()
     window.show()
 
